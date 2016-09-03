a7e0bbb12f6b4a87cb2035f58aa8ad7713ef5f23
Юлия Исаева <isaeva-spb@inbox.ru>
2016-09-03 19:17:29 +0400

    На проверку

---
+++ js/script-full.js
@@ -0,0 +1,16 @@
+var link = document.querySelector(".feedback-btn");
+var popup = document.querySelector(".modal-content");
+var close = document.querySelector(".modal-content-close");
+var overlay = document.querySelector(".modal-overlay");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("modal-content-show");
+  overlay.classList.add("modal-overlay-show");
+});
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("modal-content-show");
+  overlay.classList.remove("modal-overlay-show");
+});

b724db36b47d2aa1fa7749ad63b3528c72186c82
Юлия Исаева <isaeva-spb@inbox.ru>
2016-09-06 00:51:07 +0400

    Правки

--- js/script-full.js
+++ js/script-full.js
@@ -4,9 +4,9 @@ var close = document.querySelector(".modal-content-close");
 var overlay = document.querySelector(".modal-overlay");
 
 link.addEventListener("click", function(event) {
-  event.preventDefault();
-  popup.classList.add("modal-content-show");
-  overlay.classList.add("modal-overlay-show");
+    event.preventDefault();
+    popup.classList.add("modal-content-show");
+    overlay.classList.add("modal-overlay-show");
 });
 
 close.addEventListener("click", function(event) {
